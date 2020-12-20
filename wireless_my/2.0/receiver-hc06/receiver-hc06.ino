fb354ffc57fc1b6b833a1d9e3bed2ea49b6f00ed
StoyanTinchev <st8tinchev@gmail.com>
2020-12-20 15:44:36 +0200

    codes

---
+++ wireless_my/2.0/receiver-hc06/receiver-hc06.ino
@@ -0,0 +1,59 @@
+#include "Mouse.h"
+#include <SoftwareSerial.h>
+
+
+
+int x, y;
+
+SoftwareSerial mySerial(9, 8); // rx, tx
+long serialA;
+void setup() 
+{
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  Mouse.begin();
+}
+
+void loop() {
+
+  if (mySerial.available() > 2)
+  {
+    serialA = mySerial.parseInt();
+    Serial.println(serialA);
+  }
+
+
+  if (20000 > serialA > 10000)
+  {
+    Serial.println("xread");
+    Serial.println(x);
+    x = serialA;
+  }
+
+
+  if (serialA < 10000)
+  {
+    Serial.println("yread");
+    y = serialA;
+    Serial.println(y);
+  }
+  Mouse.move(x, y);
+
+
+  if (serialA == 70000)
+  {
+    Mouse.press(MOUSE_LEFT);
+    delay(200);
+  }
+  else if (serialA == 80000)
+    Mouse.release(MOUSE_LEFT);
+    
+  else if (serialA == 90000)
+  {
+    Mouse.press(MOUSE_RIGHT);
+    delay(100);
+    Mouse.release(MOUSE_RIGHT);
+    delay(200);
+  }
+  delay(10);
+}
