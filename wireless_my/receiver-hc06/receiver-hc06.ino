a38d00f9a9c10b2299e6d92ea8178b6d0471c1df
StoyanTinchev <st8tinchev@gmail.com>
2020-12-30 22:03:56 +0200

    Receiver new code

--- wireless_my/receiver-hc06/receiver-hc06.ino
+++ wireless_my/receiver-hc06/receiver-hc06.ino
@@ -15,35 +15,42 @@ void setup()
 
 void loop() {
 
-  if (mySerial.available() > 0)
-    serialA = mySerial.parseInt();
-
-
-  if (serialA<1200 && serialA > 800)
+  for(int i=0; i<2; ++i)
   {
-    Serial.println("xread");
-    Serial.println(x);
-    x = serialA-1000;
+    if (mySerial.available() > 0)
+      serialA = mySerial.parseInt();
+    if(serialA>1200)
+      break;
+    if (serialA<1200 && serialA > 800)
+    {
+      Serial.println("xread");
+      Serial.println(x);
+      x = serialA-1000;
+      
+    }
+    else if (serialA<700 && serialA > 300)
+    {
+      Serial.println("yread");
+      Serial.println(y);
+      y = serialA-500;
+    }
+    Mouse.move(x, y);
   }
-  else if (serialA<700 && serialA > 300)
-  {
-    Serial.println("yread");
-    Serial.println(y);
-    y = serialA-500;
-  }
-  Mouse.move(x, y);
-
 
   if (serialA == 2000)
+  {
     Mouse.press(MOUSE_LEFT);
+  }
     
-  if (serialA == 3000)
+  else if (serialA == 3000)
+  {
     Mouse.release(MOUSE_LEFT);
+  }
+    
     
   else if (serialA == 4000)
   {
     Mouse.press(MOUSE_RIGHT);
     Mouse.release(MOUSE_RIGHT);
   }
-  delay(10);
 }
