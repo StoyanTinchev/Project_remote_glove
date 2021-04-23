a7a53814288766152178e40be3dfa794aa41e638
Stoyan Tinchev <st8tinchev@gmail.com>
2021-04-23 23:54:07 +0300

    reformat code

--- wireless_implementation/receiver-hc06/receiver-hc06.ino
+++ wireless_implementation/receiver-hc06/receiver-hc06.ino
@@ -6,51 +6,51 @@ SoftwareSerial mySerial(14, 15); // rx, tx
 
 int x, y;
 long serialA;
-void setup() 
+void setup()
 {
-  Serial.begin(57600);
-  mySerial.begin(57600);
-  Mouse.begin();
+	Serial.begin(57600);
+	mySerial.begin(57600);
+	Mouse.begin();
 }
 
 void loop() {
 
-  for(int i=0; i<2; ++i)
-  {
-    if (mySerial.available() > 0)
-      serialA = mySerial.parseInt();
-      
-    if(serialA>1200)
-      break;
-      
-    if (serialA<1200 && serialA > 800)
-    {
-      Serial.println("xread");
-      Serial.println(x);
-      x = serialA-1000;
-    }
-    else if (serialA<700 && serialA > 300)
-    {
-      Serial.println("yread");
-      Serial.println(y);
-      y = serialA-500;
-    }
-  }
-  Mouse.move(x, y);
-  if (serialA == 2000)
-  {
-    Mouse.press(MOUSE_LEFT);
-  }
-    
-  else if (serialA == 3000)
-  {
-    Mouse.release(MOUSE_LEFT);
-  }
-    
-    
-  else if (serialA == 4000)
-  {
-    Mouse.press(MOUSE_RIGHT);
-    Mouse.release(MOUSE_RIGHT);
-  }
+	for (int i = 0; i < 2; ++i)
+	{
+		if (mySerial.available() > 0)
+			serialA = mySerial.parseInt();
+
+		if (serialA > 1200)
+			break;
+
+		if (serialA < 1200 && serialA > 800)
+		{
+			Serial.println("xread");
+			Serial.println(x);
+			x = serialA - 1000;
+		}
+		else if (serialA < 700 && serialA > 300)
+		{
+			Serial.println("yread");
+			Serial.println(y);
+			y = serialA - 500;
+		}
+	}
+	Mouse.move(x, y);
+	if (serialA == 2000)
+	{
+		Mouse.press(MOUSE_LEFT);
+	}
+
+	else if (serialA == 3000)
+	{
+		Mouse.release(MOUSE_LEFT);
+	}
+
+
+	else if (serialA == 4000)
+	{
+		Mouse.press(MOUSE_RIGHT);
+		Mouse.release(MOUSE_RIGHT);
+	}
 }
--- wireless_implementation/transceiver-hc05/transceiver-hc05.ino
+++ wireless_implementation/transceiver-hc05/transceiver-hc05.ino
@@ -6,7 +6,7 @@
 #define button1 6
 #define button2 7
 //int flag_left=0;
-int flag_right=0;
+int flag_right = 0;
 
 SoftwareSerial bt(14, 15);
 MPU6050 mpu;
@@ -17,48 +17,48 @@ int vx, vy;
 
 void setup()
 {
-  bt.begin(57600);
-  Serial.begin(57600);
-  Wire.begin();
+	bt.begin(57600);
+	Serial.begin(57600);
+	Wire.begin();
 
-  mpu.initialize();
+	mpu.initialize();
 }
 
 void loop()
 {
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  vx = (gx) / 300;
-  vy = (gy) / 300;
+	mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	vx = (gx) / 300;
+	vy = (gy) / 300;
 
-  Serial.print("        | X = ");
-  bt.print("        | X = ");
-  bt.print(vx + 1000);
-  Serial.print(vx + 1000);
-  Serial.print(" | Y = ");
-  bt.print(" | Y = ");
-  bt.print(vy + 500);
-  Serial.println(vy + 500);
+	Serial.print("        | X = ");
+	bt.print("        | X = ");
+	bt.print(vx + 1000);
+	Serial.print(vx + 1000);
+	Serial.print(" | Y = ");
+	bt.print(" | Y = ");
+	bt.print(vy + 500);
+	Serial.println(vy + 500);
 
-  if (digitalRead(button1) == HIGH)
-  {
-    bt.print("LEFT");
-    bt.print(2000);
-  }
-  else if (digitalRead(button1) == LOW)
-  {
-    bt.print("LEFT");
-    bt.print(3000);
-  }
-  if (digitalRead(button2) == HIGH && flag_right == 0) 
-  {
-    bt.print("RIGHT"); 
-    bt.print(4000);
-    flag_right=1;
-  }
-  else if(digitalRead(button2) == LOW && flag_right == 1)
-  {
-    bt.print("release");
-    flag_right=0;
-  }
-  delay(7);
+	if (digitalRead(button1) == HIGH)
+	{
+		bt.print("LEFT");
+		bt.print(2000);
+	}
+	else if (digitalRead(button1) == LOW)
+	{
+		bt.print("LEFT");
+		bt.print(3000);
+	}
+	if (digitalRead(button2) == HIGH && flag_right == 0)
+	{
+		bt.print("RIGHT");
+		bt.print(4000);
+		flag_right = 1;
+	}
+	else if (digitalRead(button2) == LOW && flag_right == 1)
+	{
+		bt.print("release");
+		flag_right = 0;
+	}
+	delay(7);
 }
